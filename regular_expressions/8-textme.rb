500ae1cf9aa8ee80bf19bdb99605657454b1a220
Kakaymi10 <m.moussa@alustudent.com>
2023-01-12 21:43:33 -0800

    TExtME

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:)(.\d+)|(?<=to:)(.\d+)|(?<=flags:)([0-9|:|-]+)/).join(,)
+x = ARGV[0].scan(/(?<=from:)(.\d+)|(?<=to:)(.\d+)|(?<=flags:)([0-9|:|-]+)/).split()
+puts x.join(',')
